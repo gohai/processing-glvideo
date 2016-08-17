gohai/processing-glvideo

commit 0451ba97331443285ac3241690c6e1a3b174e474

Add flags to GLVideo and a NO_SYNC flag for GoPro

examples/CustomPipelineGoPro/CustomPipelineGoPro.pde
@@ -12,7 +12,7 @@ GLVideo video;
 void setup() {
   size(320, 240, P2D);
   // we connect to the camera via this custom GStreamer pipeline
-  video = new GLVideo(this, "udpsrc port=8554 ! tsdemux ! decodebin");
+  video = new GLVideo(this, "udpsrc port=8554 ! tsdemux ! decodebin", NO_SYNC);
   video.play();
 }
 

src/gohai/glvideo/GLCapture.java
@@ -29,8 +29,8 @@ import processing.core.*;
  */
 public class GLCapture extends GLVideo {
 
-  public GLCapture(PApplet parent, int index) {
-    super(parent);
+  public GLCapture(PApplet parent, int index, int flags) {
+    super(parent, flags);
     String pipeline;
 
     if (PApplet.platform == LINUX) {
@@ -41,9 +41,13 @@ public class GLCapture extends GLVideo {
       throw new RuntimeException("Currently not supported on Windows");
     }
 
-    handle = gstreamer_open_pipeline(pipeline + index);
+    handle = gstreamer_open_pipeline(pipeline + index, flags);
     if (handle == 0) {
       throw new RuntimeException("Could not open capture device");
     }
+  }
+
+  public GLCapture(PApplet parent, int index) {
+    this(parent, index, 0);
   }
 }

src/gohai/glvideo/GLMovie.java
@@ -32,12 +32,8 @@ public class GLMovie extends GLVideo {
 
   protected String uri;
 
-  public GLMovie(PApplet parent, String fn_or_uri) {
-    this(parent, fn_or_uri, 0);
-  }
-
   public GLMovie(PApplet parent, String fn_or_uri, int flags) {
-    super(parent);
+    super(parent, flags);
 
     if (fn_or_uri.indexOf("://") != -1) {
       uri = fn_or_uri;
@@ -49,6 +45,10 @@ public class GLMovie extends GLVideo {
     if (handle == 0) {
       throw new RuntimeException("Could not load video");
     }
+  }
+
+  public GLMovie(PApplet parent, String fn_or_uri) {
+    this(parent, fn_or_uri, 0);
   }
 
   protected String filenameToUri(String fn) {

src/gohai/glvideo/GLVideo.java
@@ -33,6 +33,7 @@ public class GLVideo extends PImage {
 
   /* flags */
   public static final int MUTE = 1;
+  public static final int NO_SYNC = 2;
 
   protected static boolean loaded = false;
   protected static boolean error = false;
@@ -56,7 +57,7 @@ public class GLVideo extends PImage {
    *  @param flags pass GLVideo.MUTE to disable audio playback
    */
 
-  public GLVideo(PApplet parent) {
+  public GLVideo(PApplet parent, int flags) {
     super(0, 0, ARGB);
     this.parent = parent;
     this.flags = flags;
@@ -92,13 +93,21 @@ public class GLVideo extends PImage {
     }
   }
 
-  public GLVideo(PApplet parent, String pipeline) {
-    this(parent);
+  public GLVideo(PApplet parent) {
+    this(parent, 0);
+  }
 
-    handle = gstreamer_open_pipeline(pipeline);
+  public GLVideo(PApplet parent, String pipeline, int flags) {
+    this(parent, flags);
+
+    handle = gstreamer_open_pipeline(pipeline, flags);
     if (handle == 0) {
       throw new RuntimeException("Could not open pipeline");
     }
+  }
+
+  public GLVideo(PApplet parent, String pipeline) {
+    this(parent, pipeline, 0);
   }
 
   public void dispose() {
@@ -322,7 +331,7 @@ public class GLVideo extends PImage {
   public static native boolean gstreamer_init();
   public static native String gstreamer_filenameToUri(String fn);
   public static native long gstreamer_open(String uri, int flags);
-  public static native long gstreamer_open_pipeline(String pipeline);
+  public static native long gstreamer_open_pipeline(String pipeline, int flags);
   public static native boolean gstreamer_isAvailable(long handle);
   public static native int gstreamer_getFrame(long handle);
   public static native void gstreamer_startPlayback(long handle);

src/native/iface.h
@@ -44,10 +44,10 @@ JNIEXPORT jlong JNICALL Java_gohai_glvideo_GLVideo_gstreamer_1open
 /*
  * Class:     gohai_glvideo_GLVideo
  * Method:    gstreamer_open_pipeline
- * Signature: (Ljava/lang/String;)J
+ * Signature: (Ljava/lang/String;I)J
  */
 JNIEXPORT jlong JNICALL Java_gohai_glvideo_GLVideo_gstreamer_1open_1pipeline
-  (JNIEnv *, jclass, jstring);
+  (JNIEnv *, jclass, jstring, jint);
 
 /*
  * Class:     gohai_glvideo_GLVideo

src/native/impl.c
@@ -252,9 +252,16 @@ init_playbin_player (GLVIDEO_STATE_T * state, const gchar * uri)
 
   g_object_set (capsfilter, "caps",
       gst_caps_from_string ("video/x-raw(memory:GLMemory),format=RGBA"), NULL);
-  g_object_set (vsink, "sync", TRUE, "silent", TRUE, "qos", TRUE,
+  g_object_set (vsink, "silent", TRUE, "qos", TRUE,
       "enable-last-sample", FALSE, "max-lateness", 20 * GST_MSECOND,
       "signal-handoffs", TRUE, NULL);
+
+  // handle NO_SYNC flag
+  if ((state->flags & 2)) {
+    g_object_set (vsink, "sync", FALSE, NULL);
+  } else {
+    g_object_set (vsink, "sync", TRUE, NULL);
+  }
 
   g_signal_connect (vsink, "preroll-handoff", G_CALLBACK (preroll_cb), state);
   g_signal_connect (vsink, "handoff", G_CALLBACK (buffers_cb), state);
@@ -330,9 +337,16 @@ init_pipeline_player (GLVIDEO_STATE_T * state, const gchar * pipeline)
 
   g_object_set (capsfilter, "caps",
       gst_caps_from_string ("video/x-raw(memory:GLMemory),format=RGBA"), NULL);
-  g_object_set (vsink, "sync", TRUE, "silent", TRUE, "qos", TRUE,
+  g_object_set (vsink, "silent", TRUE, "qos", TRUE,
       "enable-last-sample", FALSE, "max-lateness", 20 * GST_MSECOND,
       "signal-handoffs", TRUE, NULL);
+
+  // handle NO_SYNC flag
+  if ((state->flags & 2)) {
+    g_object_set (vsink, "sync", FALSE, NULL);
+  } else {
+    g_object_set (vsink, "sync", TRUE, NULL);
+  }
 
   g_signal_connect (vsink, "preroll-handoff", G_CALLBACK (preroll_cb), state);
   g_signal_connect (vsink, "handoff", G_CALLBACK (buffers_cb), state);
@@ -477,13 +491,13 @@ JNIEXPORT jlong JNICALL Java_gohai_glvideo_GLVideo_gstreamer_1open
   }
 
 JNIEXPORT jlong JNICALL Java_gohai_glvideo_GLVideo_gstreamer_1open_1pipeline
-  (JNIEnv * env, jclass cls, jstring _pipeline) {
+  (JNIEnv * env, jclass cls, jstring _pipeline, jint flags) {
     GLVIDEO_STATE_T *state = malloc (sizeof (GLVIDEO_STATE_T));
     if (!state) {
       return 0L;
     }
     memset (state, 0, sizeof (*state));
-    // state->flags = flags;
+    state->flags = flags;
     state->rate = 1.0f;
 
     // setup context sharing

commit 434b99080fab2fb699b9f3f511e07c90af09efa7

Fix bug introduced in 0451ba9

examples/CustomPipelineGoPro/CustomPipelineGoPro.pde
@@ -12,7 +12,7 @@ GLVideo video;
 void setup() {
   size(320, 240, P2D);
   // we connect to the camera via this custom GStreamer pipeline
-  video = new GLVideo(this, "udpsrc port=8554 ! tsdemux ! decodebin", NO_SYNC);
+  video = new GLVideo(this, "udpsrc port=8554 ! tsdemux ! decodebin", GLVideo.NO_SYNC);
   video.play();
 }
 